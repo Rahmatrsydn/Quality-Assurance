d2db95abd39646bc63de0088dfc8046fdb239b8c
Rahmatrsydn <rahmatrsydn@gmail.com>
2024-08-15 22:43:50 +0700

    ini percobaan cherry pick

--- sesi3_RahmatRasyidin.js
+++ sesi3_RahmatRasyidin.js
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 const Bintang = 5;
 
 for(let i = 0; i < Bintang; i++)
+=======
+const Baris = 5;
+
+for(let i = 0; i < Baris; i++)
+>>>>>>> 93b1cdb (second commit)
 {
     let Ulang = "";
     for(let j = 0; j <= i; j++)
